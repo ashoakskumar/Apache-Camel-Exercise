a0faf2fb9d78bb8f38d063448777caa114a44cf4
ashoaks <mail2ashokid@gmail.com>
2021-07-04 08:53:28 +0530

    Created Sample project 2

---
+++ Apache-Camel-Exercise-2/src/main/java/com/ashok/example/ApacheCamelExercise2Application.java
@@ -0,0 +1,13 @@
+package com.ashok.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCamelExercise2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheCamelExercise2Application.class, args);
+	}
+
+}
